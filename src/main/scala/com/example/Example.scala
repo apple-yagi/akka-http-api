18957237f97d37932a666d6674b337f3ab51efa0
apple-yagi <applesryu@gmail.com>
2022-09-04 01:18:32 +0900

    Unbind route when user presses return

--- src/main/scala/com/example/Example.scala
+++ src/main/scala/com/example/Example.scala
@@ -6,6 +6,9 @@ import akka.http.scaladsl.Http
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
 
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.io.StdIn
+
 object Example {
   def main(args: Array[String]): Unit = {
 
@@ -16,6 +19,11 @@ object Example {
         complete("Hello world")
       }
 
-    Http().newServerAt("0.0.0.0", 8080).bind(route)
+    val bindingFuture = Http().newServerAt("0.0.0.0", 8080).bind(route)
+    println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
+    StdIn.readLine() // let it run until user presses return
+    bindingFuture
+      .flatMap(_.unbind()) // trigger unbinding from the port
+      .onComplete(_ => system.terminate()) // and shutdown when done
   }
 }
